01fecd703279e9209ae4c134e3b9345d3f57534f
Рева Лев Сергеевич <l.reva@office.sspk.spb.ru>
2023-03-10 11:22:05 +0300

    Add Task_2

---
+++ 1_Seminar/Task_2.py
@@ -0,0 +1,24 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+
+while(True):
+    number = input("Введите трехзначное число: ")
+
+    if number.isdigit() == False:
+        print("Введите число!")
+    else:
+        break
+if int(number) < 100 or int(number) > 999:
+    print("Введите трехзначное число!")
+
+result = 0
+initNumber = number
+sequenceNums = ''
+
+while(int(number) != 0):
+    result = result + int(number) % 10
+    number = int(number) / 10
+print(initNumber, '->', result, '(',initNumber[0], '+', initNumber[1], '+', initNumber[2],')')
