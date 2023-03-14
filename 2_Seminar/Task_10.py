325229b1dacbfd967644e5ea423027eb963639ca
Рева Лев Сергеевич <l.reva@office.sspk.spb.ru>
2023-03-14 09:32:53 +0300

    Add Task_10 Fix

--- 2_Seminar/Task_10.py
+++ 2_Seminar/Task_10.py
@@ -1,21 +1,19 @@
+import random
+
 while(True):
     n = input("Введите количество монеток: ")
     if(n.isdigit() == False):
         print("Введите число или неотрицательное число")
+    elif(int(n) <= 0):
+        print("Введите число больше 0")
     else:
         break
 
 coin_sides = list()
 
 for i in range(int(n)):
-    while(True):
-        t = input("Введите число: ")
-        if(t.isdigit() == False or (int(t) != 0 and int(t) != 1)):
-            print("Введите число (0 или 1)")
-        else:
-            coin_sides.append(int(t))
-            break
-
+    coin_sides.append(random.randint(0, 1))
+    
 if(coin_sides.count(1) < coin_sides.count(0)):
     print(n,' -> ',*coin_sides)
     print(coin_sides.count(1))
