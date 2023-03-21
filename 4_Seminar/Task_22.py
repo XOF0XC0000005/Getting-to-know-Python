5c8e9f4355fd11265e33af6733ed3d8a1b1e091a
Рева Лев Сергеевич <l.reva@office.sspk.spb.ru>
2023-03-21 11:49:13 +0300

    Add Task_22 Additional

--- 4_Seminar/Task_22.py
+++ 4_Seminar/Task_22.py
@@ -14,24 +14,18 @@ import random
 
 first_subsequence_length = input("Введите длину первой последовательности: ")
 second_subsequence_length = input("Введите дину второй последовательности: ")
-dict_subsequence = {}
-result = []
 
 first_subsequence = [random.randint(1, 20) for _ in range(int(first_subsequence_length))]
 second_subsequence = [random.randint(1, 20) for _ in range(int(second_subsequence_length))]
 
-
-for i in range(len(first_subsequence)):
-    if dict_subsequence.get(first_subsequence[i]) == None:
-        dict_subsequence[first_subsequence[i]] = 1
-
-for i in range(len(second_subsequence)):
-    if dict_subsequence.get(second_subsequence[i]) == 1:
-        dict_subsequence[second_subsequence[i]] += 1
-        result.append(second_subsequence[i])
-result.sort()
+first_subsequence_set = set(first_subsequence)
+second_subsequence_set = set(second_subsequence)
+result_subsequence = first_subsequence_set.intersection(second_subsequence_set)
 
 print(first_subsequence)
 print("----------------")
 print(second_subsequence)
-print(*result)
+if(len(result_subsequence) == 0):
+    print("Нет совпадений")
+else:
+    print(*sorted(result_subsequence))
